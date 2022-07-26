6f3712bfde3adec8daf4151b9ec3b889d2a51309
Samuel Lima <samuel.lima@voffice.com.br>
2022-07-26 15:14:40 +1200

    implementado e configurado zuul api gateway

--- hr-eureka-server/src/main/java/com/udemy/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/udemy/hreurekaserver/HrEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.hreurekaserver;
+package com.udemy.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hr-eureka-server/src/test/java/com/udemy/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/udemy/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.hreurekaserver;
+package com.udemy.hreurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
