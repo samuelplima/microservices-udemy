0c9a91b2d65c483d7a09174407266b5104eb713c
Samuel Lima <samuel.lima@voffice.com.br>
2022-07-26 18:25:59 +1200

    implementado actuator for runtime config refresh

--- hr-worker/src/main/java/com/udemy/hrworker/controller/WorkersController.java
+++ hr-worker/src/main/java/com/udemy/hrworker/controller/WorkersController.java
@@ -5,6 +5,7 @@ import com.udemy.hrworker.service.WorkerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 @RequiredArgsConstructor
 @Slf4j
+@RefreshScope
 public class WorkersController {
     private final WorkerService workerService;
 
