252826071518531f3be3ccc2ae82f749ac2b2d85
Samuel Lima <samuel.lima@voffice.com.br>
2022-07-26 14:19:23 +1200

    implementado hysxtrix para tolerancia a falhas

--- hr-payroll/src/main/java/com/udemy/hrpayroll/controller/PayrollController.java
+++ hr-payroll/src/main/java/com/udemy/hrpayroll/controller/PayrollController.java
@@ -1,9 +1,13 @@
 package com.udemy.hrpayroll.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.udemy.hrpayroll.model.dto.PayrollDTO;
+import com.udemy.hrpayroll.model.entities.Payroll;
 import com.udemy.hrpayroll.services.PayrollService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import static com.udemy.hrpayroll.helper.PayrollHelper.builderWorkerDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,10 +16,15 @@ public class PayrollController {
 
     private final PayrollService payrollService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentByIdAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public PayrollDTO getPaymentById(@PathVariable Long workerId, @PathVariable Integer days) {
         return payrollService.getPayment(workerId, days);
     }
 
+    public PayrollDTO getPaymentByIdAlternative(@PathVariable Long workerId, @PathVariable Integer days){
+        Payroll payroll = new Payroll("Bramm", 150.0, days);
+        return builderWorkerDTO(payroll);
+    }
 
 }
